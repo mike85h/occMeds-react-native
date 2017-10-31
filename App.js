e5d9fc904aa4803e5e4a8b62acefba437a8694d7
Michael Harrington <mike85h@gmail.com>
2017-10-31 16:49:24 -0700

    Added occMeds logo to screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image source={require('./assets/om_icon_white.png')} style={{height:50, width:50}}/>>
         <Text style={styles.white}>OccMeds Mobile App</Text>
         <Text style={styles.white}>More Coming Soon!</Text>
         <Text style={styles.white}>email mharrington@stmarysnds for questions</Text>
