8e12a801ddbc95abfb9feb92147eaba7b9e54e2f
Michael Harrington <mike85h@gmail.com>
2017-11-27 17:01:27 -0700

    onpress event handles user input

--- App.js
+++ App.js
@@ -1,29 +1,53 @@
-
-
-
-
-
 import React, { Component } from 'react';
 import {
   AppRegistry, 
   StyleSheet, 
   Text, 
   View,
+  Button
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import Login from './src/components/Login/Login';
+import Login from './app/components/Login/Login';
+
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Login'
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+        <Login />
+    ) 
+  }
+}
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Logged In'
+  };
+  render() {
+    return (
+      <View>
+        <Text>Hello You Are Logged IN!</Text>
+      </View>
+    );
+  }
+}
+
+export const OccMeds = StackNavigator({
+  Login: { screen: LoginScreen },
+  Home: { screen: HomeScreen }
+});
 
 
 export default class App extends React.Component {  
   render() {
-    return (
-      <Login /> 
-    )
+    return <OccMeds />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   }
 });
--- app/components/Login/LoginForm.js
+++ app/components/Login/LoginForm.js
@@ -1,22 +1,31 @@
  import React, { Component } from 'react';
  import { StyleSheet, View, TextInput, Text, TouchableOpacity, StatusBar } from 'react-native';
  import Footer from '../Footer/Footer';
+ import { StackNavigator } from 'react-navigation'; 
 
  export default class LoginForm extends Component {
-     render() {
+    _loginCall = () => {
+        const payload = {
+            username: this.username
+        }
+        console.log(payload)
+    } 
+    
+    render() {
          return(
              <View style={styles.container}>
                 <StatusBar 
-                    barStyle='light-content'
+                    //barStyle='light-content'
                 />
                 <TextInput
+                    onChangeText={(text) => this.username = text}
                     returnKeyType='done'
                     placeholder="Username" 
                     placeholderTextColor='rgba(255,255,255,0.9)'
                     style={styles.input}
                     keyboardType='email-address'>
                 </TextInput>
-                <TouchableOpacity style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.buttonContainer} onPress={this._loginCall}>
                     <Text style={styles.buttonText}>
                         Login
                     </Text>
@@ -35,13 +44,13 @@
         height: 50,
         backgroundColor: 'rgba(255,255,255, 0.7)',
         marginBottom: 20,
-        bottom: 20
+        bottom: 45
     },
     buttonContainer: {
         backgroundColor: 'rgba(175,175,175, 0.8)',
         paddingVertical: 15,
         height: 50,
-        bottom: 20,
+        bottom: 55,
     },
     buttonText: {
         color: '#fff',
