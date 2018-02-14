47e837073e5342ab94af22fc9ac38e61eba674d4
Michael Harrington <mike85h@gmail.com>
2018-02-14 15:41:55 -0700

    all myprofile fields present and linked.

--- app/components/MyProfileForm.js
+++ app/components/MyProfileForm.js
@@ -10,15 +10,19 @@ import { Actions } from 'react-native-router-flux'
 export default class MyProfileForm extends Component {
    constructor(props) {
        super(props);
-       this.state = {};
+       this.state = {
+           email: this.props.props.email,
+           code: this.props.props.code,
+           fName: this.props.props.fname,
+           lName: this.props.props.lname,
+           username: this.props.props.username
+       };
        this.logProps = this.logProps.bind(this)
     }
 
 
    logProps() {
-       console.log(this.props)
-       console.log(this.state)
-       //console.log(this.state)
+      
    }
 
    render() {
@@ -26,41 +30,72 @@ export default class MyProfileForm extends Component {
             <View style={styles.container}>
                 <Header />
                     <View style={styles.profileForm}>
-                        <Text>{this.props.props.email}</Text>
-                        <TextInput></TextInput>
-                        <TouchableOpacity>
-                           <Text>
-                               Edit Email
-                           </Text>
-                        </TouchableOpacity>
-                        <Text>{this.props.props.code}</Text>
-                        <TextInput></TextInput>
-                        <TouchableOpacity>
-                            <Text>
-                                Edit Code
-                            </Text>
-                        </TouchableOpacity>
-                        <Text>{this.props.props.lname}</Text>
-                        <TextInput></TextInput>
-                        <TouchableOpacity>
-                            <Text>
-                                Edit Last Name
-                            </Text>
-                        </TouchableOpacity>
-                        <Text>{this.props.props.fname}</Text>
-                        <TextInput></TextInput>
-                        <TouchableOpacity>
-                            <Text>
-                                Edit First Name
-                            </Text>
-                        </TouchableOpacity>
-                        <Text>{this.props.props.username}</Text>
-                        <TextInput></TextInput>
-                        <TouchableOpacity>
-                            <Text>
-                                Edit Username
-                            </Text>
-                        </TouchableOpacity>
+                        <View>
+                            <Text>{this.state.email}</Text>
+                            <TextInput
+                              style={styles.inputField}
+                              placeholder='enter new email address'
+                              returnKeyLabel = {'Done'}
+                              onChangeText = {(text)=> this.setState({email: text})}/>
+                            <TouchableOpacity 
+                              style={styles.saveButton}>
+                                <Text>
+                                    Save Email
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <Text>{this.state.code}</Text>
+                            <TextInput
+                              style={styles.inputField}
+                              placeholder='enter new code'
+                              returnKeyLabel = {'Done'}
+                              onChangeText = {(text)=> this.setState({code: text})}/>
+                            <TouchableOpacity style={styles.saveButton}>
+                                <Text>
+                                    Save Code
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <Text>{this.state.lName}</Text>
+                            <TextInput
+                              style={styles.inputField}
+                              placeholder='enter new last name'
+                              returnKeyLabel = {'Done'}
+                              onChangeText = {(text)=> this.setState({lName: text})}/>
+                            <TouchableOpacity style={styles.saveButton}>
+                                <Text>
+                                    Save Last Name
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <Text>{this.state.fName}</Text>
+                            <TextInput
+                              style={styles.inputField}
+                              placeholder='enter new first name'
+                              returnKeyLabel = {'Done'}
+                              onChangeText = {(text)=> this.setState({fName: text})}/>
+                            <TouchableOpacity style={styles.saveButton}>
+                                <Text>
+                                    Save First Name
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <Text>{this.state.username}</Text>
+                            <TextInput
+                              style={styles.inputField}
+                              placeholder='enter new username'
+                              returnKeyLabel = {'Done'}
+                              onChangeText = {(text)=> this.setState({username: text})}/>
+                            <TouchableOpacity style={styles.saveButton}>
+                                <Text>
+                                    Save Username
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 <Footer />
             </View>
@@ -73,10 +108,16 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     profileForm: {
-        backgroundColor: 'rgb(255,255,255)',
+        backgroundColor: 'rgb(255,255,2)',
         marginTop: 200,
         height: 200,
         width: 200,
         justifyContent: 'center'
+    },
+    inputField: {
+        backgroundColor: 'rgb(240,120,100)'
+    },
+    saveButton: {
+        backgroundColor: 'rgb(20,100,100)'
     }
 });
